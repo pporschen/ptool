4ddafc48b01e433c3351c0f9697974c2c93bccbc
Paddy <accounts@pp83.de>
2024-05-17 17:25:46 +0200

    refactor: Update App component structure and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,18 @@
-import { AppBar, ThemeProvider, styled } from "@mui/material";
-import Box from "@mui/material/Box";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import theme from "./consts/theme";
+import Header from "./components/Header";
+import ContentWrapper from "./components/ContentWrapper";
 
 function App() {
 	return (
 		<ThemeProvider theme={theme}>
-			<Box>
-				<StyledAppBar position="static">
-					<Logo>painpointer.io</Logo>
-				</StyledAppBar>
-			</Box>
+			<>
+				<CssBaseline />
+				<Header />
+				<ContentWrapper />
+			</>
 		</ThemeProvider>
 	);
 }
 
 export default App;
-
-const Logo = styled(Box)`
-	font-size: 1.5em;
-	font-family: "Poetsen One", sans-serif;
-	background: linear-gradient(to right, #20b2aa, #e303cd);
-	-webkit-background-clip: text;
-	-webkit-text-fill-color: transparent;
-	margin: ${({ theme }) => theme.spacing(2)};
-`;
-
-const StyledAppBar = styled(AppBar)`
-	background-color: ${({ theme }) => theme.palette.background.default};
-`;
