abbaf4a3dac8e41300551311c67a048b123a1e50
Paddy <accounts@pp83.de>
2024-05-17 17:03:14 +0200

    Add AppBar and theme to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+import { AppBar, ThemeProvider, styled } from "@mui/material";
+import Box from "@mui/material/Box";
+import theme from "./consts/theme";
+
 function App() {
-	return <div className="App">app test 2345</div>;
+	return (
+		<ThemeProvider theme={theme}>
+			<Box>
+				<StyledAppBar position="static">
+					<Logo>painpointer.io</Logo>
+				</StyledAppBar>
+			</Box>
+		</ThemeProvider>
+	);
 }
 
 export default App;
+
+const Logo = styled(Box)`
+	font-size: 1.5em;
+	font-family: "Poetsen One", sans-serif;
+	background: linear-gradient(to right, #20b2aa, #e303cd);
+	-webkit-background-clip: text;
+	-webkit-text-fill-color: transparent;
+	margin: ${({ theme }) => theme.spacing(2)};
+`;
+
+const StyledAppBar = styled(AppBar)`
+	background-color: ${({ theme }) => theme.palette.background.default};
+`;
---
+++ src/consts/theme.ts
@@ -0,0 +1,20 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: "#556cd6",
+		},
+		secondary: {
+			main: "#19857b",
+		},
+		error: {
+			main: "#ff3333",
+		},
+		background: {
+			default: "#f5f5f5",
+		},
+	},
+});
+
+export default theme;
