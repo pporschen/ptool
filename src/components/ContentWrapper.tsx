378628caee6189aa5694e94e1ecd0444b125f7a1
Paddy <accounts@pp83.de>
2024-05-17 17:49:03 +0200

    style: Refactor ContentWrapper component to use Material-UI styling and add Card components

--- src/components/ContentWrapper.tsx
+++ src/components/ContentWrapper.tsx
@@ -1,9 +1,23 @@
+import { Box, Card, Container, styled } from "@mui/material";
+
 const ContentWrapper = () => {
 	return (
-		<div className="content-wrapper">
-			<h1>Content Wrapper</h1>
-		</div>
+		<Container fixed sx={{ marginTop: (theme) => theme.spacing(3) }}>
+			<Box display="flex" gap={2}>
+				<StyledCard>
+					<p>Content goes here...</p>
+				</StyledCard>
+				<StyledCard>
+					<p>Content goes here...</p>
+				</StyledCard>
+			</Box>
+		</Container>
 	);
 };
 
 export default ContentWrapper;
+
+const StyledCard = styled(Card)`
+	padding: ${({ theme }) => theme.spacing(3)};
+	width: 100%;
+`;
