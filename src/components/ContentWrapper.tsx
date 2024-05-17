a4d30b53663991d257b32867372e862dc41f7a1e
Paddy <accounts@pp83.de>
2024-05-18 00:17:02 +0200

    refactor: Add form controls for pain level and body part selection in ContentWrapper component

--- src/components/ContentWrapper.tsx
+++ src/components/ContentWrapper.tsx
@@ -1,18 +1,61 @@
-import { Box, Button, Card, CardActions, Container, styled } from "@mui/material";
+import {
+	Box,
+	Button,
+	Card,
+	CardActions,
+	Container,
+	FormControl,
+	InputLabel,
+	MenuItem,
+	Select,
+	SelectChangeEvent,
+	styled,
+} from "@mui/material";
 import Front from "./svgs/Front";
 import Back from "./svgs/Back";
 import Left from "./svgs/Left";
 import Right from "./svgs/Right";
-import { useState } from "react";
+import { ReactNode, useState } from "react";
+import theme from "../consts/theme";
+
+enum PainLevels {
+	none = 0,
+	mild = 1,
+	moderate = 2,
+	severe = 3,
+}
+
+enum BodyParts {
+	fullBody,
+	head,
+	teeth,
+	eye,
+	feet,
+	hands,
+}
+
+type FormData = {
+	painlevel: PainLevels;
+	bodyPart: BodyParts;
+};
 
 const ContentWrapper = () => {
 	const [currentPerspective, setCurrentPerspective] = useState(0);
+	const [formData, setFormData] = useState<FormData>({ painlevel: PainLevels.none, bodyPart: BodyParts.fullBody });
+
 	const perspectives = [
 		{ name: "Front", svg: <Front /> },
 		{ name: "Right", svg: <Right /> },
 		{ name: "Back", svg: <Back /> },
 		{ name: "Left", svg: <Left /> },
 	];
+
+	const handleFormChange = (e: SelectChangeEvent<unknown>, _: ReactNode) => {
+		const { name, value } = e.target;
+		console.log(e.target);
+		setFormData({ ...formData, [name]: value });
+	};
+
 	return (
 		<Container fixed sx={{ marginTop: (theme) => theme.spacing(3) }}>
 			<Box display="flex" gap={2}>
@@ -32,7 +75,40 @@ const ContentWrapper = () => {
 					</CardActions>
 				</StyledCard>
 				<StyledCard>
-					<p>Content goes here...</p>
+					<FormControl fullWidth margin="normal">
+						<InputLabel id="body-part-label">Body Part</InputLabel>
+						<Select
+							labelId="body-part-label"
+							id="body-part-select"
+							value={formData.bodyPart.toString()}
+							name="bodyPart"
+							label="Body Part"
+							onChange={handleFormChange}
+						>
+							<MenuItem value={BodyParts.fullBody}>Full Body</MenuItem>
+							<MenuItem value={BodyParts.head}>Head</MenuItem>
+							<MenuItem value={BodyParts.teeth}>Teeth</MenuItem>
+							<MenuItem value={BodyParts.eye}>Eye</MenuItem>
+							<MenuItem value={BodyParts.feet}>Feet</MenuItem>
+							<MenuItem value={BodyParts.hands}>Hands</MenuItem>
+						</Select>
+					</FormControl>
+					<FormControl fullWidth margin="normal">
+						<InputLabel id="pain-level-label">Pain Level</InputLabel>
+						<Select
+							labelId="pain-level-label"
+							id="pain-level-select"
+							value={formData.painlevel.toString()}
+							name="painlevel"
+							label="Pain Level"
+							onChange={handleFormChange}
+						>
+							<MenuItem value={PainLevels.none}>Itch</MenuItem>
+							<MenuItem value={PainLevels.mild}>Mild Pain</MenuItem>
+							<MenuItem value={PainLevels.moderate}>Moderate Pain</MenuItem>
+							<MenuItem value={PainLevels.severe}>Severe Pain</MenuItem>
+						</Select>
+					</FormControl>
 				</StyledCard>
 			</Box>
 		</Container>
