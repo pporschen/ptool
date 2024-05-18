334103586a51e6d7fdf3fef2f91242f4e6fff969
Paddy <accounts@pp83.de>
2024-05-18 12:46:52 +0200

    feat: Add translations

---
+++ src/i18n.ts
@@ -0,0 +1,30 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import resources from "./consts/translations";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+const formatBrowserLanguage = (lng: string) => {
+	if (lng.includes("-")) {
+		return lng.split("-")[0];
+	}
+	return lng;
+};
+
+i18n
+	.use(LanguageDetector)
+	.use(initReactI18next) // passes i18n down to react-i18next
+	.init({
+		resources,
+		fallbackLng: "en",
+		supportedLngs: ["de", "en"],
+		interpolation: {
+			escapeValue: false, // react already safes from xss
+		},
+		detection: {
+			order: ["querystring", "cookie", "localStorage", "sessionStorage", "navigator", "htmlTag", "path", "subdomain"],
+			caches: ["localStorage"],
+			convertDetectedLanguage: formatBrowserLanguage,
+		},
+	});
+
+export default i18n;
