9990a8ea1a1cbc522da836f540b71ba91abff352
Paddy <accounts@pp83.de>
2024-05-18 15:01:36 +0200

    refactor: Improve GazeButton component for gaze input interaction

--- src/components/GazeButton.tsx
+++ src/components/GazeButton.tsx
@@ -1,4 +1,5 @@
 import { Button, ButtonProps, LinearProgress } from "@mui/material";
+import { use } from "i18next";
 import { set } from "lodash";
 import { useRef, useState } from "react";
 
@@ -9,34 +10,42 @@ type GazeButtonProps = {
 
 const DWELLING_TIME_LIMIT = 1500;
 const REFRESH_RATE = 500;
-const dwellingTimeToProgress = (dwellingTime: number) => (dwellingTime / DWELLING_TIME_LIMIT) * 100;
+const DWELLING_DELAY = 500;
+const ACTION_DELAY = 500;
+const dwellingTimeToProgress = (dwellingTime: number) => ((dwellingTime - DWELLING_DELAY) / DWELLING_TIME_LIMIT) * 100;
 
 const GazeButton = ({ gazeInputIsEnabled, onClick, ...props }: GazeButtonProps) => {
 	const [dwellingTime, setDwellingTime] = useState(0);
 	const [isDwelling, setIsDwelling] = useState(false);
+	const dwellingDelayPassed = dwellingTime >= DWELLING_DELAY;
 
 	const timer = useRef<NodeJS.Timeout | null>(null);
 	const actionTimeout = useRef<NodeJS.Timeout | null>(null);
 
 	const handleGazeEntry = () => {
+		setDwellingTime(0);
+		if (timer.current) {
+			clearInterval(timer.current);
+		}
 		if (gazeInputIsEnabled) {
 			timer.current = setInterval(() => {
 				setDwellingTime((prev) => {
 					const next = prev + REFRESH_RATE;
-					return Math.min(next, DWELLING_TIME_LIMIT);
+					return Math.min(next, DWELLING_TIME_LIMIT + DWELLING_DELAY);
 				});
 			}, REFRESH_RATE);
 			setIsDwelling(true);
 			actionTimeout.current = setTimeout(() => {
 				onClick();
-			}, DWELLING_TIME_LIMIT + 500);
+				setIsDwelling(false);
+			}, DWELLING_TIME_LIMIT + DWELLING_DELAY + ACTION_DELAY);
 		}
 	};
 
 	const handleGazeLeave = () => {
 		if (gazeInputIsEnabled) {
 			if (timer.current) {
-				clearTimeout(timer.current);
+				clearInterval(timer.current);
 			}
 			if (actionTimeout.current) {
 				clearTimeout(actionTimeout.current);
@@ -48,7 +57,7 @@ const GazeButton = ({ gazeInputIsEnabled, onClick, ...props }: GazeButtonProps) 
 
 	return (
 		<Button onClick={onClick} onMouseEnter={handleGazeEntry} onMouseLeave={handleGazeLeave} {...props}>
-			{isDwelling && (
+			{isDwelling && dwellingDelayPassed && (
 				<LinearProgress
 					variant="determinate"
 					value={dwellingTimeToProgress(dwellingTime)}
@@ -56,7 +65,7 @@ const GazeButton = ({ gazeInputIsEnabled, onClick, ...props }: GazeButtonProps) 
 					sx={{ width: "100%" }}
 				/>
 			)}
-			{!isDwelling && props.children}
+			{(!isDwelling || !dwellingDelayPassed) && props.children}
 		</Button>
 	);
 };
