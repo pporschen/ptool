c41234fd8289dae9db20970afe5a48689ae39650
Paddy <accounts@pp83.de>
2024-05-19 20:07:25 +0200

    feat: Add SVG components for heads and bodies

---
+++ src/components/svgs/index.ts
@@ -0,0 +1,31 @@
+import HeadBack from "./Head/HeadBack";
+import HeadFront from "./Head/HeadFront";
+import HeadTop from "./Head/HeadTop";
+import HeadLeft from "./Head/HeadLeft";
+import HeadRight from "./Head/HeadRight";
+
+import MaleBodyFront from "./Bodies/Male/MaleBodyFront";
+import MaleBodyBack from "./Bodies/Male/MaleBodyBack";
+import MaleBodyLeft from "./Bodies/Male/MaleBodyLeft";
+import MaleBodyRight from "./Bodies/Male/MaleBodyRight";
+
+import FemaleBodyFront from "./Bodies/Female/FemaleBodyFront";
+import FemaleBodyBack from "./Bodies/Female/FemaleBodyBack";
+import FemaleBodyLeft from "./Bodies/Female/FemaleBodyLeft";
+import FemaleBodyRight from "./Bodies/Female/FemaleBodyRight";
+
+export {
+	HeadBack,
+	HeadFront,
+	HeadTop,
+	HeadLeft,
+	HeadRight,
+	MaleBodyFront,
+	MaleBodyBack,
+	MaleBodyLeft,
+	MaleBodyRight,
+	FemaleBodyFront,
+	FemaleBodyBack,
+	FemaleBodyLeft,
+	FemaleBodyRight,
+};
